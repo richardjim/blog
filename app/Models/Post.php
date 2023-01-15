22eccabffb923e5c5c6c5e101b3506aa33fa0f69
richardjim <richardiyevhobu@gmail.com>
2023-01-15 01:20:14 +0100

    implement setup softdelete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
     use HasFactory;
     protected $fillable = [
         'title',
@@ -14,6 +17,8 @@ class Post extends Model
         'content',
         'category_id',
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function category()
     {
